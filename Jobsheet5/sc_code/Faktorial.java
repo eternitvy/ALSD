c4dca09ce5e18c34bac2dfa48f8e99663cd05059
eternitvy <crrissant@gmail.com>
2025-03-30 00:03:00 +0700

    percobaan 1

--- Jobsheet5/sc_code/Faktorial.java
+++ Jobsheet5/sc_code/Faktorial.java
@@ -1,9 +1,17 @@
 package sc_code;
 public class Faktorial {
     int faktorialBF(int n) {
+        // int fakto = 1;
+        // for (int i = 1; i <= n; i++) {
+        //     fakto = fakto * i;
+        // }
+
+        // pertanyaan No.3
         int fakto = 1;
-        for (int i = 1; i <= n; i++) {
+        int i = 1;
+        while (i <= n) {
             fakto = fakto * i;
+            i++;
         }
         return fakto;
     }
